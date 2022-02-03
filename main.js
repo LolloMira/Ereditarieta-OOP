89bcc3f2dc8aab38a25beeadfcaacc1c21aad19b
LolloMira <lorenzo23082000@gmail.com>
2022-02-03 13:17:00 +0100

    Update

--- main.js
+++ main.js
@@ -3,4 +3,6 @@ let pointD = new Point(7, 3);
 
 const mediaPoint = PuntoMedio.medianPoint(pointC, pointD);
 
+
+
 console.log(mediaPoint);
--- mathC.js
+++ mathC.js
@@ -12,4 +12,17 @@ class PuntoMedio{
         const distance = this.calculateDistance(origin, p);
         return distance;
     }
+
+    static nearestPoint(startringPoint, ...points){
+        let nearest = points[0];
+
+        for (let i = 1; i < points.length; i++) {
+            const point = points[i];
+            if (this.calculateDistance(startingPoint, point) < this.calculateDistance(startingPoint, nearest)) {
+             nearest = point;   
+            }
+            
+        }
+        return nearest;
+    }
 }
