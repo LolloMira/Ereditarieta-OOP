2b8442fdb118f457fb6f3a03acdbb8da29ea850f
LolloMira <lorenzo23082000@gmail.com>
2022-02-07 11:00:27 +0100

    update

--- main.js
+++ main.js
@@ -1,56 +1,14 @@
+const segment1 = new Segment(3, 5, 6, 1);
 
-const pointA = new Point(3, 5);
+console.log(segment1.length);
+console.log(segment1.mediumPoint);
 
-const pointB = new Point(6, 1);
+const verticeInfSx = new Point(3, 2);
+const verticeSupSx = new Point(3, 6);
+const verticeSupDx = new Point(8, 2);
+const verticeInfDx = new Point(8, 6);
 
-const pointC = new Point(7, 1);
+const rectangle1 = new Rectangle(verticeInfSx, verticeSupSx, verticeSupDx, verticeInfDx);
 
- HEAD
- HEAD
-console.log(mediaPoint);
-
-
- e013a79424ea3467232174b646b52f4fbce0f0c1
-const pointD = new Point(3, 6);
-
-const pointE = new Point(1, 1);
-
-const pointF = new Point(-2, 1);
-
-
-const distance = MathC.calculateDistance(pointA, pointB);
-
-const medianPoint = MathC.medianPoint(pointA, pointB);
-
-const distanceFromOrigin = MathC.distanceFromOrigin(pointA);
-
-const nearest = MathC.nearestPoint(pointA, pointB, pointC, pointD, pointE, pointF);
-
-
-console.log(distance);
-
-console.log(medianPoint);
-
-
-const distanceFromB = pointA.distanceFrom(pointB);
-
-console.log(distanceFromB);
-
-const distanceFromOriginOfA = pointA.distanceFromOrigin();
-
-console.log(distanceFromOriginOfA);
-
-const medianFromB = pointA.medianPointFromPoint(pointB);
-
-console.log(medianFromB);
-
-const nearestToA = pointA.nearestPoint(pointB, pointC, pointD, pointE);
-
-console.log(nearestToA);
-
- HEAD
-// let bev2 = new Bevanda(name: "chinotto", insertionDate: new Date())
- e013a79424ea3467232174b646b52f4fbce0f0c1
-
-// let bev2 = new Bevanda(name: "chinotto", insertionDate: new Date())
- e013a79424ea3467232174b646b52f4fbce0f0c1
+console.log(rectangle1.perimetro);
+console.log(rectangle1.area);
