33e9fb4cae063b45090beaedb95bf46d836ba3b4
LolloMira <lorenzo23082000@gmail.com>
2022-02-07 08:44:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,51 @@
-let pointC = new Point(6, 3);
-let pointD = new Point(7, 3);
 
-const mediaPoint = PuntoMedio.medianPoint(pointC, pointD);
+const pointA = new Point(3, 5);
 
+const pointB = new Point(6, 1);
 
+const pointC = new Point(7, 1);
 
+<<<<<<< HEAD
 console.log(mediaPoint);
+
+=======
+const pointD = new Point(3, 6);
+
+const pointE = new Point(1, 1);
+
+const pointF = new Point(-2, 1);
+
+
+const distance = MathC.calculateDistance(pointA, pointB);
+
+const medianPoint = MathC.medianPoint(pointA, pointB);
+
+const distanceFromOrigin = MathC.distanceFromOrigin(pointA);
+
+const nearest = MathC.nearestPoint(pointA, pointB, pointC, pointD, pointE, pointF);
+
+
+console.log(distance);
+
+console.log(medianPoint);
+
+
+const distanceFromB = pointA.distanceFrom(pointB);
+
+console.log(distanceFromB);
+
+const distanceFromOriginOfA = pointA.distanceFromOrigin();
+
+console.log(distanceFromOriginOfA);
+
+const medianFromB = pointA.medianPointFromPoint(pointB);
+
+console.log(medianFromB);
+
+const nearestToA = pointA.nearestPoint(pointB, pointC, pointD, pointE);
+
+console.log(nearestToA);
+
+
+// let bev2 = new Bevanda(name: "chinotto", insertionDate: new Date())
+>>>>>>> e013a79424ea3467232174b646b52f4fbce0f0c1
--- mathC.js
+++ mathC.js
@@ -1,28 +1,56 @@
-class PuntoMedio{
+class MathC{
 
-    static medianPoint(c, d){
-        const xMed = (c.x + d.x) / 2;
-        const yMed = (c.y + d.y) / 2;
+    static calculateDistance(a, b){
 
-        const puntoMedio = new Point(xMed, yMed)
-        return puntoMedio;
+        const xDif = b.x - a.x;
+
+        const xDif2 = xDif * xDif;
+
+        const yDif = b.y - a.y;
+
+        const yDif2 = yDif * yDif;
+
+        const sumDif2 = xDif2 + yDif2;
+
+        const sqrtSumDif2 = Math.sqrt(sumDif2);
+
+        return sqrtSumDif2;
+
+
+        // return Math.sqrt(Math.pow((b.x - a.x),2) + Math.pow((b.y - a.y),2))
     }
+
+    static medianPoint(a, b){
+
+        const xM = (a.x + b.x)/2
+        const yM = (a.y + b.y)/2
+
+        const median = new Point(xM, yM);
+
+        return median;
+    }
+
+
     static distanceFromOrigin(p){
-        const origin = new Point(0, 0);
+        const origin = new Point(0,0);
         const distance = this.calculateDistance(origin, p);
         return distance;
     }
 
-    static nearestPoint(startringPoint, ...points){
+
+    static nearestPoint(startingPoint, ...points){
         let nearest = points[0];
 
-        for (let i = 1; i < points.length; i++) {
+        for (let i = 1; i  < points.length; i++) {
             const point = points[i];
             if (this.calculateDistance(startingPoint, point) < this.calculateDistance(startingPoint, nearest)) {
-             nearest = point;   
+                nearest = point;
             }
-            
         }
+
         return nearest;
+
+
+        // return points.reduce(p, c => this.calculateDistance(startingPoint, p) < this.calculateDistance(startingPoint, c) ? p : c);
     }
 }
